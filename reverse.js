0df2fd6914b5adbc3452c447048c15e9ccfd0085
ITProfessor40148 <58896542+ITProfessor40148@users.noreply.github.com>
2020-01-15 00:24:00 +0800

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -4,11 +4,11 @@
  */
 var reverse = function(x) {
     let res = 0;
-    if(x < 0) {
+    if (x < 0) {
         x = Math.abs(x);
         res = -Number(String(x).split('').reverse().join(''));
     } else {
-        res = Number(String(x).split('').reverse().join(''));   
+        res = Number(String(x).split('').reverse().join(''));
     }
     if (res < (Math.pow(2, 31) * -1) || res > Math.pow(2, 31) - 1) {
         return 0;
