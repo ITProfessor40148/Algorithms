2caa346579b7bdc87292e2c40cc3033260439139
ITProfessor40148 <58896542+ITProfessor40148@users.noreply.github.com>
2020-01-14 11:14:11 +0800

    reverse.js-2020.1.14

---
+++ reverse.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let res = 0;
+    if(x < 0) {
+        x = Math.abs(x);
+        res = -Number(String(x).split('').reverse().join(''));
+    } else {
+        res = Number(String(x).split('').reverse().join(''));   
+    }
+    if (res < (Math.pow(2, 31) * -1) || res > Math.pow(2, 31) - 1) {
+        return 0;
+    }
+    return res;
+};
